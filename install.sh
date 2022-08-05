b8b1a8a6fd7b169d09a41007b7b799f64fc392dc
Adarsh keshan <adkeshan@amazon.com>
2022-08-05 12:48:28 -0500

    testing

--- install.sh
+++ install.sh
@@ -25,6 +25,3 @@ apt-get install silversearcher-ag
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-
-

e285cf498ff51f042badb6f23f15c9629b0da01e
adarshkeshan <keshanadarsh@gmail.com>
2024-03-27 19:07:35 -0700

    Update build_tmux.sh

--- tmux/build_tmux.sh
+++ tmux/build_tmux.sh
@@ -1,18 +1,26 @@
-# Steps to build and install tmux from source on Ubuntu.
-# Takes < 25 seconds on EC2 env [even on a low-end config instance].
-VERSION=2.5
-sudo apt-get -y remove tmux
-sudo apt-get -y install wget tar libevent-dev libncurses-dev
-wget https://github.com/tmux/tmux/releases/download/${VERSION}/tmux-${VERSION}.tar.gz
-tar xf tmux-${VERSION}.tar.gz
-rm -f tmux-${VERSION}.tar.gz
-cd tmux-${VERSION}
-./configure
+# Install tmux 3.0a on Centos
+
+# install deps
+sudo yum install -y gcc kernel-devel make ncurses-devel
+
+# DOWNLOAD SOURCES FOR LIBEVENT AND MAKE AND INSTALL
+curl -LOk https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stable.tar.gz
+tar -xf libevent-2.1.11-stable.tar.gz
+cd libevent-2.1.11-stable
+./configure --prefix=/usr/local
 make
 sudo make install
-cd -
-sudo rm -rf /usr/local/src/tmux-*
-sudo mv tmux-${VERSION} /usr/local/src
 
-## Logout and login to the shell again and run.
-## tmux -V
+# DOWNLOAD SOURCES FOR TMUX AND MAKE AND INSTALL
+
+curl -LOk https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
+tar -xf tmux-3.0a.tar.gz
+cd tmux-3.0a
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
+make
+sudo make install
+
+# pkill tmux
+# close your terminal window (flushes cached tmux executable)
+# open new shell and check tmux version
+tmux -V

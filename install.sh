2baf23a612c10a41e59b625e8e06ee529bf74dae
adarshkeshan <keshanadarsh@gmail.com>
2024-03-27 19:00:23 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall
 
